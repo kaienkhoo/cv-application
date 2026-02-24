659babf418d2b60236e5cfc8abb37ebf01b2dda0
kaienkhoo <100989619+kaienkhoo@users.noreply.github.com>
2026-02-24 15:51:19 +0800

    Updated Education.jsx

--- src/components/Education.jsx
+++ src/components/Education.jsx
@@ -1,6 +1,6 @@
 import { useState } from "react";
 
-function Educations() {
+function Educations({ isEditing }) {
     const [schoolName, setSchoolName] = useState('');
     const [title, setTitle] = useState('');
     const [date, setDate] = useState('');
@@ -8,9 +8,20 @@ function Educations() {
     return (
         <section className="education-section">
             <h2>Educational Experience</h2>
-            <label htmlFor='schoolName'>School Name:<input id='schoolName' type="text" placeholder="Enter your school name" value={schoolName} onChange={(e) => setSchoolName(e.target.value)}></input></label>
-            <label htmlFor='title'>Title:<input id='title' type="text" placeholder="Enter title of study" value={title} onChange={(e) => setTitle(e.target.value)}></input></label>
-            <label htmlFor='date'>Date:<input id='date' type="text" placeholder="Enter date of study" value={date} onChange={(e) => setDate(e.target.value)}></input></label>
+            {isEditing ? (
+                <div className='inputs-group'>
+                    <label htmlFor='schoolName'>School Name:<input id='schoolName' type="text" placeholder="Enter your school name" value={schoolName} onChange={(e) => setSchoolName(e.target.value)} required></input></label>
+                    <label htmlFor='title'>Title:<input id='title' type="text" placeholder="Enter title of study" value={title} onChange={(e) => setTitle(e.target.value)} required></input></label>
+                    <label htmlFor='date'>Date:<input id='date' type="date" placeholder="Enter date of study" value={date} onChange={(e) => setDate(e.target.value)} required></input></label>
+                </div>
+            ) : (
+                <div className="view-group">
+                    <p><strong>School Name:</strong> {schoolName}</p>
+                    <p><strong>Title of Study:</strong> {title}</p>
+                    <p><strong>Date of Study:</strong> {date}</p>
+                </div>
+            )}
+
         </section>
     )
 }
